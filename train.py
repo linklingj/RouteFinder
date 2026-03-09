469a4418180f93b3b0efe9ec0cc88888a8602814
linklingj <jaehyunc413@gmail.com>
2026-03-09 22:32:55 +0900

    update req

--- train.py
+++ train.py
@@ -1,16 +1,19 @@
 from ultralytics import YOLO
+import torch
 
 def main():
+    print("PyTorch version:", torch.__version__)
+    print("CUDA available:", torch.cuda.is_available())
 
-    model = YOLO("yolo26l-seg.pt")
+    # model = YOLO("yolo26l-seg.pt")
 
-    model.train(
-        data='data.yaml',
-        epochs=10,
-        batch=16,
-        imgsz=640,
-        device=0
-    )
+    # model.train(
+    #     data='data.yaml',
+    #     epochs=10,
+    #     batch=16,
+    #     imgsz=640,
+    #     device=0
+    # )
 
 if __name__ == "__main__":
     main()
